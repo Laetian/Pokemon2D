5995d0dc3d6e3746572db0ff1125501aee77f9bd
Laetian <laetianv@gmail.com>
2021-09-11 22:24:12 +0200

    Adde player movement

--- Pokymon/Assets/Scripts/Player/PlayerController.cs
+++ Pokymon/Assets/Scripts/Player/PlayerController.cs
@@ -16,12 +16,28 @@ public class PlayerController : MonoBehaviour
             input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
             //input.x = Input.GetAxisRaw("Horizontal");
             //input.y = Input.GetAxisRaw("Vertical");
+            if (input.x !=0)
+            {
+                input.y = 0;
+            }
             if(input != Vector2.zero)
             {
                 var targetPosition = transform.position;
                 targetPosition.x += input.x;
                 targetPosition.y += input.y;
+                StartCoroutine(MoveTowards(targetPosition));
             }
         }
+    }
+    IEnumerator MoveTowards(Vector3 destination)
+    {
+        isMoving = true;
+        while(Vector3.Distance(transform.position, destination) > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = destination;
+        isMoving = false;
     }
 }

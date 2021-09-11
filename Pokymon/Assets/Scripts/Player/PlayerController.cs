ebde6a04896a5c4be28703625592d9a89c7c969a
Laetian <laetianv@gmail.com>
2021-09-11 21:56:41 +0200

    Added layers

---
+++ Pokymon/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool isMoving;
+    [SerializeField]
+    private float speed;
+}

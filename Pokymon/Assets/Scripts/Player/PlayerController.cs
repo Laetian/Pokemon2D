29e1ffd31c6722a0572128d45ded012933b9346b
Laetian <laetianv@gmail.com>
2021-09-11 22:08:35 +0200

    Added colliders to SolidObjects

--- Pokymon/Assets/Scripts/Player/PlayerController.cs
+++ Pokymon/Assets/Scripts/Player/PlayerController.cs
@@ -7,4 +7,21 @@ public class PlayerController : MonoBehaviour
     private bool isMoving;
     [SerializeField]
     private float speed;
+    private Vector2 input;
+
+    private void Update()
+    {
+        if(!isMoving)
+        {
+            input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            //input.x = Input.GetAxisRaw("Horizontal");
+            //input.y = Input.GetAxisRaw("Vertical");
+            if(input != Vector2.zero)
+            {
+                var targetPosition = transform.position;
+                targetPosition.x += input.x;
+                targetPosition.y += input.y;
+            }
+        }
+    }
 }

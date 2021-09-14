79abe6323889ee59790c0a8d1215870cf143e1bb
Laetian <laetianv@gmail.com>
2021-09-14 20:57:59 +0200

    Added animator to player and configured idle states

--- Pokymon/Assets/Scripts/Player/PlayerController.cs
+++ Pokymon/Assets/Scripts/Player/PlayerController.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     private bool isMoving;
     [SerializeField]
     private float speed;
     private Vector2 input;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
@@ -22,6 +30,8 @@ public class PlayerController : MonoBehaviour
             }
             if(input != Vector2.zero)
             {
+                _animator.SetFloat("MoveX", input.x);
+                _animator.SetFloat("MoveY", input.y);
                 var targetPosition = transform.position;
                 targetPosition.x += input.x;
                 targetPosition.y += input.y;

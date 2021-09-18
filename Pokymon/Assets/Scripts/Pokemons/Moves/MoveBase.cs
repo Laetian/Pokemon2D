69090b1c487603eaac32beb62e4b2b31693a760c
Laetian <laetianv@gmail.com>
2021-09-18 22:53:41 +0200

    Added moves and finished MoveBase.cs

--- Pokymon/Assets/Scripts/Pokemons/Moves/MoveBase.cs
+++ Pokymon/Assets/Scripts/Pokemons/Moves/MoveBase.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="Move", menuName ="Pokemon/Nuevo Movimiento")]
 public class MoveBase : ScriptableObject
 {
-    [SerializeField] private string name;
+    [SerializeField] private string moveName;
+    public string MoveName => moveName;
     [TextArea] [SerializeField] private string description;
     [SerializeField] private PokemonType type;
+    public PokemonType Type => type;
     [SerializeField] private int power;
-    [SerializeField] private int accurazy;
+    public int Power => power;
+    [SerializeField] private int accuracy;
+    public int Accuracy => accuracy;
     [SerializeField] private int pp;//number of times can be used
+    public int Pp => pp;
 }
